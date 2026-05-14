296917044706dbd865f084c6b229529afaae1b3f
aurelio <aurelioserafim2807@gmail.com>
2026-05-14 17:59:11 -0300

    made the class object store the coefficients

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -1,10 +1,26 @@
 import numpy as np
 
+# Todo
+#   - check if all the entries are equal so we dont divide by zero
+#   - implement mse
+#   - implement rmse
+#   - implement mae
+#   - adjusted r-squared
+
 
 class SimpleLinearRegression:
+    """
+    This class describes the main Simple Linear Regression Model
+        - Calculates B0 and B1
+        - Calculates the estimated y (yhat)
+        - Calcululates RSS
+        - Calculates R-Squared
+    """
+
     def __init__(self):
-        b0 = 0
-        b1 = 0
+        self.b0_hat = None
+        self.b1_hat = None
+        self.y_hat = None
 
     def b1(self, xi: np.array, yi: np.array) -> float:
         x_mean = np.mean(xi)
@@ -14,39 +30,48 @@ class SimpleLinearRegression:
         y_mean = np.mean(yi)
         return y_mean - b1 * np.mean(xi)
 
-    def rss(self, yi: np.array, yihat: np.array) -> float:
-        return np.sum((yi - yihat) ** 2)
+    def rss(self, yi: np.array, yhat: np.array) -> float:
+        return np.sum((yi - yhat) ** 2)
 
-    def yihat(self, b0: float, b1: float, xi: np.array) -> np.array:
+    def rsquared(self, yi: np.array, yhat: np.array) -> float:
+        y_mean = np.mean(yi)
+        return 1 - (np.sum((yi - yhat) ** 2)) / np.sum((yi - y_mean) ** 2)
+
+    def yhat(self, b0: float, b1: float, xi: np.array) -> np.array:
         return b0 + b1 * xi
 
-    def fit(self, X_train, y_train):
-        b1 = self.b1(X_train, y_train)
-        b0 = self.b0(b1, X_train, y_train)
-        yihat = self.yihat(b0, b1, X_train)
-        rss = self.rss(y_train, yihat)
-        rsquared = self.rsquared(y_train, yihat)
-        print(f"Preds : {yihat}")
-        print(f"Residual Sum of Squares: {rss}")
-        print(f"R squared: {rsquared}")
+    def standard_errors(self, b0: float, b1: float) -> float:
+        pass
 
-    def rsquared(self, y_train: np.array, yihat: np.array) -> float:
-        y_mean = np.mean(y_train)
-        return (np.sum(y_train - y_mean) - np.sum((y_train - yihat) ** 2)) / np.sum(
-            (y_train - y_mean) ** 2
-        )
+    def fit(self, X_train, y_train):
+        self.b1_hat = self.b1(X_train, y_train)
+        self.b0_hat = self.b0(self.b1_hat, X_train, y_train)
+        self.y_hat = self.yhat(self.b0_hat, self.b1_hat, X_train)
+        rsquared = self.rsquared(y_train, self.y_hat)
+        print(f"The R2 of the fit {rsquared}")
+        return self.y_hat
+
+    def score(self, X_test: np.array, y_test: np.array) -> float:
+        y_pred = self.predict(X_test)
+        return self.rsquared(y_test, y_pred)
 
     def predict(self, X_test: np.array) -> np.array:
-        # preds = self.yihat(b0, self.b1, X_test)
-        # return preds
-        pass
+        y_pred = self.yhat(self.b0_hat, self.b1_hat, X_test)
+        return y_pred
 
 
 if __name__ == "__main__":
-    X_train = np.array([1, 2])
-    y_train = np.array([3, 7.6])
-    X_test = np.array([3, 8])
-    y_test = np.array([6, 13.3])
+    X_train = np.array([1, 2, 4, 5, 6, 6, 7, 7])
+    y_train = np.array([3, 7.6, 9.5, 9.8, 11, 11, 12.75, 12.75])
+    X_test = np.array([3, 8, 9])
+    y_test = np.array([6, 13.3, 11.5])
+
     lr = SimpleLinearRegression()
+
     lr.fit(X_train, y_train)
-    lr.predict(X_test)
+
+    rsquared = lr.score(X_test, y_test)
+
+    # if r2 is negative it means it is worse that just predicting the average of yi
+
+    print(f"The R2 of the fit: {rsquared}")
